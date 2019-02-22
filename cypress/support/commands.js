a538b98d6d2011249ff6025ea81b0999d0ca3ec7
student <student@safebear.co.uk>
2019-02-22 10:37:33 +0000

    Playing with requests.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// Cypress will send HTTP requests to check to see if the product is alread present in the list. If it is, delete it.
+Cypress.Commands.add("deleteProducts", (product) => {
+
+            var responseBody;
+            
+            // Store the response JSON of the product list
+            cy.request('http://localhost:3000/api/v1/products')
+                .then((response) => {
+                    responseBody = response.body;
+                })
+            
+            for (var productJson in responseBody){
+                if(productJson.prod_name == product.name){
+                     // Send a DELETE request to create the computer
+                    cy.request('DELETE', 'http://localhost:3000/api/v1/products/' + productJson._id)
+                    // Check that this was accepted by the server (200 ok)
+                    .should((response) => {
+                        expect(response.status).to.eq(200)
+                     })
+
+                }
+            }
+    })
